508c039fe5dfe624e46d1ad317cc664145884da7
Farid Andre <fduarteinfo17@gmail.com>
2023-03-05 20:11:01 -0500

    Ejercicios de Logica

---
+++ ejercicios/bisiesto.py
@@ -0,0 +1,13 @@
+def is_leap(year):
+    # Write your logic here
+    leap = False
+    if year % 400 != 0 and year % 100 == 0:
+        leap = False
+    elif year % 4 == 0:
+        leap = True
+
+    return leap
+
+
+year = int(input())
+print(is_leap(year))
